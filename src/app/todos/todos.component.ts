883cc710630474871e4a7d8f848bbb026f91d6fc
Javid Rashid lone <jlone@in-mac-33.corp.yodlee.com>
2017-07-31 18:54:55 +0530

    added remainingtodos

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -12,6 +12,10 @@ import { TodoService } from "./todos.service"
         .todolist-container {
                 min-height:900px;
                 background: #c0c0c0;
+         }
+        #sortable li {
+                border:1px solid red;
+        }       
             `
      ],
      providers : [TodoService]
@@ -23,15 +27,77 @@ export class TodosComponent implements OnInit {
     todos : Todo[];
     todo: any
     todoName: string;
+    status : boolean = false;
+    alreadyDone = false;
+    remainingTodos: any;
 
     constructor(private _todosService : TodoService) {}
 
     ngOnInit() : void{
         this.todos = this._todosService.getTodos();
+        console.info(this._todosService.getTodos());
+        this._todosService.getTodos().forEach(function(elem, i) {
+            var self = this;
+            var remainingTodos = 0;
+             if(elem.status === true) {
+                
+              remainingTodos += remainingTodos
+           }
+       })
     }
     addAToDo(todo: Todo[]) : void {
-        console.log(this.todos);
-        console.log({todoName : this.todo});
-        this.todos.push({todoName: this.todo});
+        console.log(todo);
+        this.todos.push({todoName : this.todo, status: false});
+        this.todo = '';
+        var remainingTodos = 0;
+        this.remainingTodos = this.todos.forEach(function(elem , i) {
+            if(elem.status === true) {
+                remainingTodos += 1;
+            }
+        });
+        console.log(remainingTodos);
+    }
+    mouseMoved(todo : Todo[]) {
+        //console.log(todo);
+        // var allTodos = this.todos;
+        // var self = this;
+        // var alreadyDone = false;
+        // allTodos.forEach(function(elem, i) {
+        //     //console.info(todo)
+        //     // if(allTodos[i].todoName == todo) {
+        //     //    self.alreadyDone = true;
+        //     // }
+        //     alreadyDone = false;
+        // })
+         
+    }
+    todoDone(todo : Todo[]) {
+        console.log(this);
+        if(this.status === false) {
+            console.log('task is incomplete');
+           this.remainingTodos = this.todos.length - 1;
+            this.todos.forEach((elem, i) => {
+                if(elem.status === true) {
+                    this.remainingTodos -= 1;
+                }
+                else {
+                    this.remainingTodos = this.remainingTodos;
+                }
+            })
+           
+            //console.log(this.remainingTodos);
+        }
+        else {
+            console.log('completed');
+        }
+         return this.remainingTodos;
+    }
+    setAllAsDone() {
+        console.log('sss');
+        this.todos.forEach(function(elem, i) {
+            console.log(elem);
+            elem.status = true;
+
+        })
     }
 }
